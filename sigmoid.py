9ead8bcdd054e48d0bd4359c1d6fd4d0b2d3781e
santosh8309 <nenavath185@gmail.com>
2017-12-06 18:53:24 +0530

    sigmoid function

---
+++ sigmoid.py
@@ -0,0 +1,18 @@
+import math
+
+while True:
+	
+	
+  x=float(input("Enter the value of 'x':"))
+  e = math.pow(2.8, -x)
+  sig = 1/(1+e)
+  print(sig)
+  s = input("Enter 'q' for exit/ENTER for again:")
+ 	
+  if s=='q':break
+ 
+
+ 
+
+	
+
